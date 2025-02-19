dbb54c7126e6487f14879af73e181359bd6fd99b
Vsevolod <woornad@gmail.com>
2025-02-20 00:42:04 +0300

    temporary example - how to wrap to async interface

--- pasio/src/pymodule.cpp
+++ pasio/src/pymodule.cpp
@@ -1,3 +1,5 @@
+#include <pybind11/attr.h>
+#include <pybind11/gil.h>
 #include <pybind11/pybind11.h>
 
 #include <memory>
@@ -5,6 +7,7 @@
 #include <pybind11/cast.h>
 #include <pybind11/functional.h>
 
+#include <pybind11/pytypes.h>
 #include <string>
 
 #include <pasio/async/async_serial_port.hpp>
@@ -14,11 +17,26 @@ namespace py = pybind11;
 
 using py::literals::operator""_a;
 
+std::string async_task(int seconds) {
+    std::this_thread::sleep_for(std::chrono::seconds(seconds)); // Simulate delay
+    return "Task completed!";
+}
+
+// Wrapper function that returns a Python coroutine (async def function)
+py::object async_function(int seconds) {
+    py::gil_scoped_acquire gil;
+    py::object loop = py::module_::import("asyncio").attr("get_event_loop")();
+    return loop.attr("run_in_executor")(py::none(), py::cpp_function(async_task), seconds);
+}
+
 PYBIND11_MODULE(pasio, async) {
-    // py::class_<pasio::sync::serial_port_wrapper>(sync, "SyncSerialPort")
-    //     .def(py::init<const std::string&, unsigned int>())
-    //     .def("write", &pasio::sync::serial_port_wrapper::write)
-    //     .def("read", &pasio::sync::serial_port_wrapper::read);
+    async.def("async_function", &async_function, "An asynchronous function", py::arg("seconds"));
+
+    py::class_<pasio::sync::serial_port_wrapper>(async, "SyncSerialPort")
+        .def(py::init<const std::string&, unsigned int>())
+        .def("write", &pasio::sync::serial_port_wrapper::write)
+        .def("read", &pasio::sync::serial_port_wrapper::read);
+
     py::class_<pasio::async::async_serial_port>(async, "AsyncSerialPort")
         .def(py::init<const std::string&, unsigned int>())
         .def("write", &pasio::async::async_serial_port::write)
